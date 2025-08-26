58437f0bf476796365cb32916dab4e4b7f276e3b
Sss <surkun1992@gmail.com>
2025-08-26 21:44:46 +0700

    Добавление сущностей Role, Permission, User. Создание миграции

--- BooksTestTask.DataAccess/Migrations/20250826143915_AddRolePermissionsentities.Designer.cs
+++ BooksTestTask.DataAccess/Migrations/20250826143915_AddRolePermissionsentities.Designer.cs
@@ -4,6 +4,7 @@ using BooksTestTask.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BooksTestTask.DataAccess.Migrations
 {
     [DbContext(typeof(BooksDbContext))]
-    partial class BooksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250826143915_AddRolePermissionsentities")]
+    partial class AddRolePermissionsentities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BooksTestTask.Model/UserRole.cs
+++ BooksTestTask.Model/UserRole.cs
@@ -4,7 +4,7 @@ public class UserRole
 {
     public int Id { get; set; }
 
-    public int UserId { get; set; }
+    public Guid UserId { get; set; }
 
     public virtual required User User { get; set; }
 
